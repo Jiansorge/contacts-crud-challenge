e8370ca9d485eca761cbedfe4d2378454eef1af1
[jiansorge] <[ji@nsorge.com]>
2021-06-26 00:38:45 -0700

    initialized CRUD functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import Contact from './components/Contact';
 import ContactList from './components/ContactList';
 
 function App() {
+  const getContacts=()=>{
+
+  }
+
+const getSingleContact=(contactId)=>{
+
+}
+
+const editContact=(contactId)=>{
+
+}
+
+const deleteContact=(contactId)=>{
+
+}
+
+const addContact=()=>{
+  
+}
+
   return (
     <div className="app">
         <ContactList/>
